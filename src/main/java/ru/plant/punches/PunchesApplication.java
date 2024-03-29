5a89463b2b2ab48d29ed282f64a3e49230eba6d1
Ivan.Vereschagin <Ivan.Vereschagin@casceng.com>
2024-03-29 20:48:03 +0300

    init

---
+++ src/main/java/ru/plant/punches/PunchesApplication.java
@@ -0,0 +1,13 @@
+package ru.plant.punches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PunchesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/plant/punches/PunchesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.plant.punches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
